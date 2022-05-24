d518e43048260d31e034415b0d8b863df4591137
artem78 <megabyte1024@ya.ru>
2022-05-24 23:46:35 +0300

    Add multiline comment to test data

--- Tests/db-updates/5.sql
+++ Tests/db-updates/5.sql
@@ -3,5 +3,8 @@ INSERT INTO t1 (str, flag, x, y) VALUES ('alpha', 1, 7, 9);
 INSERT INTO t1 (str, flag, x, y) VALUES ('beta', 0, 9, 3);
 INSERT INTO t1 (str, flag, x, y) VALUES ('gamma', 0, 3, 1);
 
+/* add some more data
+   to another table */
+
 INSERT INTO t2 (id, a, b) VALUES (1, 11, 15);
 INSERT INTO t2 (id, a, b) VALUES (2, 9, 6);
